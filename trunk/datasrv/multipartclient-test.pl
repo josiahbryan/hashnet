bf6d8f9561daafa2c25aae1fb596c3ac1fb468e4
josiahbryan <josiahbryan@4a3a74ab-0cd2-2cf1-66bf-38c0e3c76fb6>
2012-09-21 21:06:04 +0000

    Tested multipart replaced responses for use in streamed TR pushing

---
+++ trunk/datasrv/multipartclient-test.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use common::sense;
+
+use IO::Socket;
+my $remote = IO::Socket::INET->new(
+			Proto    => "tcp",
+			PeerAddr => "localhost",
+			PeerPort => "8051",
+		)
+|| die "can't connect to 8051 PORT on localhost";
+
+print $remote "GET /db/ver HTTP/1.0\n\n";
+
+my $boundary = undef;
+my $got_boundary = 0;
+while (!$got_boundary && ($_ = <$remote>))
+{
+	if(/multipart\/x-mixed-replace/)
+	{
+		$boundary =~ /boundary="?.*"?$/;
+		print "Boundary: $boundary\n";
+		$got_boundary = 1;
+	}
+	else
+	{
+		#print "Not boundary:[$_]\n";
+	}
+}
+
+my @buffer = ();
+my $hit_page = 0;
+while(<$remote>)
+{
+	push @buffer, $_;
+	if(/--$boundary/)
+	{
+		shift @buffer while $buffer[0] =~ /^\s*[\r\n]+$/;
+		shift @buffer if $buffer[0] =~ /--$boundary/;
+		#print "[[$buffer[0]]]\n\n\n";
+		shift @buffer while $buffer[0] =~ /^\s*[\r\n]+$/;
+		my $ctype = undef;
+		if($buffer[0] =~ /content-type: (.*)$/i)
+		{
+			$ctype = $1;
+			shift @buffer;
+		}
+		shift @buffer while $buffer[0] =~ /^[\r\n]+$/;
+		
+		pop @buffer if @buffer[$#buffer] =~ /--$boundary/;
+		pop @buffer while $buffer[$#buffer] =~ /^\s*[\r\n]+$/;
+		
+		#shift @buffer; shift @buffer; pop @buffer; pop @buffer;
+		print "$ctype [",join("",@buffer),"]\n" if @buffer;
+		@buffer = ();
+	}
+}
