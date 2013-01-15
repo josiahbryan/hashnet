1fa318d82e2bbbe87c8397bcb99d3348411e4cd2
josiahbryan <josiahbryan@4a3a74ab-0cd2-2cf1-66bf-38c0e3c76fb6>
2013-01-15 01:54:24 +0000

    Updated Peer.pm to add support for SSH tunnels

--- trunk/devel/netnode/lib/HashNet/MP/Peer.pm
+++ trunk/devel/netnode/lib/HashNet/MP/Peer.pm
@@ -74,6 +74,54 @@
 		my $self = shift;
 		my $host = $self->{host};
 		return undef if !$host;
+
+		
+
+		if($host =~ /^sshtun:(.*?):(.*?)@(.*?)\/(.*?):(.*)$/)
+		{
+			# Example: sshtun:root:+rootpw.txt@mypleasanthillchurch.org/localhost:8031
+			my %args = (
+				user 	=> $1,
+				pass	=> $2,
+				tunhost	=> $3,
+				host	=> $4,
+				port	=> $5
+			);
+
+			if($args{pass} =~ /^\+(.*)$/)
+			{
+				my $file = $1;
+				warn "Peer: _open_socket: sshtun: Password file '$file' does not exist" if !-f $file;
+				$args{pass} = read_file($file);
+				$args{pass} =~ s/[\r\n]//g;
+			}
+
+			my $local_port = 60000;
+			$local_port ++ while $local_port < 65536 and `lsof -i :$local_port`;
+			if($local_port == 65536)
+			{
+				error "Peer: _open_socket: sshtun: Cannot find an open port for the local end of the tunnel\n";
+			}
+
+			$host = "localhost:${local_port}";
+
+			use HashNet::Util::MyNetSSHExpect;
+
+			my $ssh_tun_arg = "-L$local_port:$args{host}:$args{port}";
+
+			trace "Peer: _open_socket: Opening SSH tunnel via $args{tunhost} to $args{host}:$args{port}, local port $local_port\n";
+			debug "Peer: _open_socket: \$ssh_tun_arg: '$ssh_tun_arg'\n";
+
+			$self->{ssh_handle} = Net::SSH::Expect->new (
+				host		=> $args{tunhost},
+				password	=> $args{user},
+				user		=> $args{pass},
+				raw_pty		=> 1,
+				ssh_option	=> $ssh_tun_arg,
+			);
+
+			$self->{ssh_handle}->login();
+		}
 		
 		my $port = 8031;
 		if($host =~ /^(.*?):(\d+)$/)
@@ -95,6 +143,12 @@
 		$handle->autoflush(1); # so output gets there right away
 		
 		return $handle;
+	}
+
+	sub DESTROY
+	{
+		my $self = shift;
+		$self->{ssh_handle}->close() if $self->{ssh_handle};
 	}
 	
 	sub open_connection
