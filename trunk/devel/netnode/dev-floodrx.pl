f0e3401bc5202ed1f1701a184d47263362f2d964
josiahbryan <josiahbryan@4a3a74ab-0cd2-2cf1-66bf-38c0e3c76fb6>
2013-01-23 16:47:20 +0000

    dev-floodrx.pl fixed, measured local (client/host on same machine) transmit rate at >200 Mb/sec for 1024 MB (that's time from start transmit loop to last ACK received)

--- trunk/devel/netnode/dev-floodrx.pl
+++ trunk/devel/netnode/dev-floodrx.pl
@@ -26,11 +26,15 @@ my $sock = $ch->sw->{sock};
 #$ch->sw->send_message({uuid => '3-'.time(), "data"=>2}, 'a' x (1024 * 1024));
 #$ch->sw->send_message({uuid => '3-'.time(), "data"=>3}, 'a' x (1024 * 1024));
 
+my $att_len = (1024 * 1024);
 my $max = 1024;
 my $time = time();
 my $len = 0;
-$len += $ch->sw->send_message({uuid => $_.'.1-'.$time, "data" => $_}, '#' x (1024 * 1024)) for 1..$max;
-        $ch->sw->wait_for_ack(         $_.'.1-'.$time) for 1..$max;
+$len += $ch->sw->send_message({uuid => $_.'.1-'.$time, "data" => $_}, '#' x $att_len) for 1..$max;
+
+$ch->sw->wait_for_ack($_.'.1-'.$time) for 1..$max;
+
+$len = $att_len * $max;# + length($_.'.1-'.$time.'data'.$_) * $max;
 
 my $time_d = time - $time;
 my $bytes_sec = $len / $time_d;
@@ -44,6 +48,7 @@ my $time = time();
 
 $ch->destroy_app;
 
+
 trace "$0: Done\n";
-trace "$0: Sent ".sprintf("%.02K", $len / 1024)." in ".sprintf("%.02s", $time_d)." at a rate of ".sprintf("%.02Kb/sec", $bytes_sec / 1024)."\n";
+trace "$0: Sent ".sprintf("%.02f MB", $len / 1024 / 1024)." in ".sprintf("%.02fs", $time_d)." at a rate of ".sprintf("%.02f MB/sec", $bytes_sec / 1024 / 1024)."\n";
 
